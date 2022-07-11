bfa71d35f8b26f64bd3eeba87860de02dce1396c
Eden-Cadagiani-Oclock <eden@oclock.io>
2022-07-11 18:30:32 +0200

    feat: link apollo provider to typeORM on recipe

--- apps/backend/src/app/graphql/providers/recipe.provider.ts
+++ apps/backend/src/app/graphql/providers/recipe.provider.ts
@@ -1,5 +1,17 @@
 import { DataSource } from 'apollo-datasource';
 import { QueryRecipeArgs } from '../generated/graphql';
+import { Recipe } from '../../entities/recipe.entity';
+
+interface createRecipeArgs {
+    title: string,
+    content: string
+}
+
+interface updateRecipeArgs extends QueryRecipeArgs{
+  title?: string,
+  content?: string
+}
+
 /**
  * This is a data source exemple which can be used with typeorm to access data.
  * This dataSource is injected into the context of the apollo server,
@@ -7,10 +19,31 @@ import { QueryRecipeArgs } from '../generated/graphql';
  */
 export class RecipesProvider extends DataSource {
   public async getRecipe({ id }: QueryRecipeArgs) {
-    return { id };
+    const recipe = await Recipe.findOneBy({id});
+    return recipe;
   }
 
   public async getRecipes() {
-    return [];
+    const recipes = await Recipe.find();
+    return recipes;
+  }
+
+
+  public async createRecipe({title, content}: createRecipeArgs) {
+    const recipe = new Recipe()
+    recipe.title = title;
+    recipe.content = content;
+    await recipe.save();
+    return recipe;
+  }
+
+  public async updateRecipe({id, title, content}: updateRecipeArgs) {
+    const recipe = await Recipe.findOneBy({id});
+    if(title)
+      recipe.title = title;
+    if(content)
+      recipe.content = content;
+    await recipe.save();
+    return recipe;
   }
 }
