73f9ebd1bc325259405c0b2727e25bca754cfaa6
Eden-Cadagiani-Oclock <eden@oclock.io>
2022-07-11 18:30:43 +0200

    feat: add CRUD to recipe

--- apps/backend/src/app/graphql/resolvers/recipe.resolver.ts
+++ apps/backend/src/app/graphql/resolvers/recipe.resolver.ts
@@ -3,10 +3,14 @@ import { QueryResolvers } from '../generated/graphql';
 // Resolvers define the technique for fetching the types in the schema.
 export const RecipeResolver: {
   Query: Pick<QueryResolvers, 'recipe' | 'recipes'>;
+  Mutation: Pick<QueryResolvers, 'createRecipe' | 'updateRecipe'>;
 } = {
   Query: {
     recipe: (_, args, ctx) => ctx.dataSources.recipes.getRecipe(args),
     recipes: (_, __, ctx) => ctx.dataSources.recipes.getRecipes(),
   },
-  // Mutation: {},
+  Mutation: {
+    createRecipe: (_, args, ctx) => ctx.dataSources.recipes.createRecipe(args),
+    updateRecipe: (_, args, ctx) => ctx.dataSources.recipes.updateRecipe(args),
+  },
 };
--- apps/backend/src/app/graphql/typeDefs/recipe.type.ts
+++ apps/backend/src/app/graphql/typeDefs/recipe.type.ts
@@ -3,5 +3,11 @@ import { gql } from 'apollo-server';
 export const RecipeTypeDefs = gql`
   type Recipe {
     id: ID
+    title: String
+    content: String
+  }
+  type Mutation {
+    createRecipe(title: String, content: String): Recipe
+    updateRecipe(id: ID, title: String, content: String): Recipe
   }
 `;
